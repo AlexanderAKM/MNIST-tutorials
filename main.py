d5ad2166d8b28628bb12c674205075526da9ab8e
AlexanderAKM <a.k.muller.2@student.rug.nl>
2024-08-14 06:29:51 +0000

    ran Advanced_MNIST

--- main.py
+++ main.py
@@ -1,7 +1,2 @@
 # git commit -m "Adjusted directory structure"
 # git push origin main
-
-#git config --global user.email "you@example.com"
-#   git config --global user.name "Your Name"
-
-# All is good?
